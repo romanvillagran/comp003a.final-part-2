9024bba1bc8a28a4e25fa622121b5cee94550bdf
Your Name <you@example.com>
2025-03-02 16:21:58 -0800

    added premium plan

--- comp003a.final part 2/PremiumMember.cs
+++ comp003a.final part 2/PremiumMember.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace comp003a.final_part_2
 {
-    internal class PremiumMember
+    public class PremiumMember : Member
     {
+        public override string Plan { get; set; } = "Premium"; // Set plan to "Premium"
+
+        //initialize a premium member
+        public PremiumMember(string name, int age) : base(name, age) { }
     }
 }
